b1db07461b0239c54d979da3f0860e9929c8d929
mumua <466232748@qq.com>
2018-04-03 22:38:21 +0800

    add project file for iview project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm'
+export default {
+  name: 'App',
+  components: {
+    LoginForm: LoginForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
